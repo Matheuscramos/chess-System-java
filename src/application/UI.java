46ee34935770adf11339d25eb9c4339fa496d08a
Matheus da Costa Ramos <matheuscramos@hotmail.com>
2024-02-27 10:09:25 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,6 +31,12 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2j");
+		System.out.flush();
+	}
+	
+	
 	//este método ira ler uma posição do usuário.
 	// o método recebe o scanner do programa principal
 	public static ChessPosition readChessPosition(Scanner sc) {
